34a0be8c91995303277e95dd0bb8abb0ad4acc54
Muhammad Yudha Kurniawan <107832923+Muhammad-Yudha-Kurniawan@users.noreply.github.com>
2023-07-27 17:15:46 +0800

    Add files via upload

---
+++ Jobless/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      screens:{
+        'phone': '320px',
+        // => @media (min-width: 320px) {... }
+        
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+      fontFamily: {
+        Poppins: ['Poppins'],
+      },
+    },
+  },
+  plugins: [],
+}
+
